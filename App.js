6065552271b11ab27096b1bb194001b6f18df0f8
Merdan <wisew.tube@gmail.com>
2025-09-29 23:58:05 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,46 +1,45 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 import HomeScreen from "./src/screens/HomeScreen";
 import SearchScreen from "./src/screens/SearchScreen";
+// import { SafeAreaView } from "react-native-safe-area-context";
 
-const TopTab = createMaterialTopTabNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <TopTab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color }) => {
-              let iconName;
+    // <SafeAreaView style={{ flex: 1 }}>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-              if (route.name === "Home") {
-                iconName = focused
-                  ? "ios-information-circle"
-                  : "ios-information-circle-outline";
-              } else if (route.name === "Search") {
-                iconName = focused ? "ios-list" : "ios-list-outline";
-              }
+            if (route.name === "Home") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline";
+            } else if (route.name === "Search") {
+              iconName = focused ? "ios-list" : "ios-list-outline";
+            }
 
-              return <Ionicons name={iconName} size={20} color={color} />;
-            },
-            headerShown: false,
-            tabBarShowIcon: true,
-            tabBarShowLabel: false,
-            tabBarActiveTintColor: "#FF6B00",
-            tabBarInactiveTintColor: "#A1A1AA",
-            tabBarIndicatorStyle: { backgroundColor: "#FF6B00" },
-            tabBarStyle: { backgroundColor: "#353535" },
-          })}
-        >
-          <TopTab.Screen name="Home" component={HomeScreen} />
-          <TopTab.Screen name="Search" component={SearchScreen} />
-        </TopTab.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          headerShown: false,
+          tabBarActiveTintColor: "#FF6B00",
+          tabBarInactiveTintColor: "#A1A1AA",
+          headerStyle: { backgroundColor: "#353535" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { color: "#fff" },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    // </SafeAreaView>
   );
 }
